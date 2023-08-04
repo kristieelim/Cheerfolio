28d0fceb432a4c87c890d7492fcc3ce4de260891
kristieelim <kristie_lim@hotmail.com>
2023-08-04 17:19:32 +0200

    Changes to database design

--- db_init.py
+++ db_init.py
@@ -16,17 +16,31 @@ CREATE TABLE IF NOT EXISTS users (
 );
 '''
 
-create_entries_table_query = '''
-CREATE TABLE IF NOT EXISTS entries (
-    entry_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+create_categories_table_query = '''
+CREATE TABLE IF NOT EXISTS categories (
+    cat_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     user_id INTEGER NOT NULL,
-    entry TEXT NOT NULL,
-    category TEXT DEFAULT "default" NOT NULL,
+    name TEXT NOT NULL,
+    goal TEXT NOT NULL,
+    image BLOB,
     FOREIGN KEY(user_id) REFERENCES users(id)
 );
 '''
 
+create_entries_table_query = '''
+CREATE TABLE IF NOT EXISTS entries (
+    entry_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    user_id INTEGER NOT NULL,
+    time TIMESTAMP NOT NULL,
+    category TEXT NOT NULL,
+    entry TEXT NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(category) REFERENCES categories(cat_id)
+);
+'''
+
 db.execute(create_users_table_query)
+db.execute(create_categories_table_query)
 db.execute(create_entries_table_query)
 
 # Commit changes and close connection
